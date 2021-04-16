e86e6069d421a9fda4f729994eab371b220ddb22
Dustin Brinkman <mimirs.spring@gmail.com>
2021-04-16 14:08:30 -0400

    models done

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,40 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const getProjects = () => {
+    return db('projects')
+}
+
+const getProjectById = id => {
+    return db('projects').where('id', id).first();
+}
+
+const getProjectTasks = projectId => {
+    return db('tasks')
+    .innerjoin('projects', 'tasks.project_id', 'projects.id')
+    .where('project_id', projectId)
+    .select([
+        'tasks.id',
+        'tasks.description',
+        'tasks.notes',
+        'tasks.completed',
+        'projects.name',
+        'projects.description'
+    ])
+}
+
+const insertProject = project => {
+    return db('projects').insert(project)
+}
+
+const insertTask = task => {
+    return db('tasks').insert(task)
+}
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    getProjectTasks,
+    insertProject,
+    insertTask,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,17 @@
 // build your `/api/projects` router here
+const express = require('express');
+const db = require('../project/model');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try{
+        const projects = await db.getProjects();
+        return res.status(200).json(projects)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:id', ( req, res, next ) => {
+    return res.status(200)
+})
