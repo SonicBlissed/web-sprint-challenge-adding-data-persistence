f6d6ba099e5aa0dcb4f4ec9e769c5cf4c9fb22d0
Dustin Brinkman <mimirs.spring@gmail.com>
2021-04-16 14:23:16 -0400

    project router complete

--- api/project/router.js
+++ api/project/router.js
@@ -1,11 +1,11 @@
 // build your `/api/projects` router here
 const express = require('express');
-const db = require('../project/model');
+const Projects = require('../project/model');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try{
-        const projects = await db.getProjects();
+        const projects = await Projects.getProjects();
         return res.status(200).json(projects)
     } catch (error) {
         next(error)
@@ -13,5 +13,39 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:id', ( req, res, next ) => {
-    return res.status(200)
+    try {
+        return res.status(200).json(req.project)
+    } catch (error) {
+        next(error)
+    }
 })
+
+router.post('/', async (req, res, next) => {
+    try {
+        await Projects.insertProject(req.body)
+        return res.status(201).json(req.body)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:id/tasks', async (req, res, next) => {
+    try {
+        const tasks = await Projects.getProjectTasks(req.params.id);
+        return res.status(200).json(tasks)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/:id/tasks', async (req, res, next) => {
+    try {
+        await Projects.insertTask(req.body)
+        return res.status(201).json(req.body)
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+module.exports = router
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,5 @@
 // build your `/api/resources` router here
+const express = require('express');
+const Resources = require('../resource/model');
+const router = express.Router();
+
