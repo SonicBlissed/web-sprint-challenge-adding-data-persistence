4390e175b0180713f3099a8b984bf696b46213fe
Dustin Brinkman <mimirs.spring@gmail.com>
2021-04-16 14:32:29 -0400

    MVP

--- api/resource/router.js
+++ api/resource/router.js
@@ -3,3 +3,22 @@ const express = require('express');
 const Resources = require('../resource/model');
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+    try{
+     const resources = await Resources.getResources();
+     return res.status(200).json(resources);
+    } catch(error) {
+        next(error)
+    }
+ })
+
+ router.post('/', async (req, res, next) => {
+    try{
+        await Resources.addResource(req.body)
+        return res.status(201).json(req.body)
+    } catch(error) {
+        next(error)
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // start your server here
+
+
+
+
+
+
+//yes I know the DB's are empty. I emptied them after testing
 const express = require('express');
 const helmet = require('helmet');
+const projectRouter = require('./api/project/router');
+const resourceRouter = require('./api/resource/router');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
+
+
 server.use(helmet());
 server.use(express.json());
+
+server.use('/projects', projectRouter);
+server.use('/resources', resourceRouter);
+
+
 server.use((req, res, next, err) => {    
     console.log(err);
 
